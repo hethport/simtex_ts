afb3363a2e990fcef0b1a1346354b670937f09a5
Herbert Baier <baier@itbaier.de>
2023-02-20 08:56:14 +0100

    xml/export interface + tools

---
+++ src/model/architecture/ExportXML.ts
@@ -0,0 +1,43 @@
+/**
+ * File:     ExportXML.java
+ * Package:  de.uniwuerzburg.zpd.tlh.parser.core
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     20.02.2023
+ */
+
+
+/**
+ * Defines xml exports.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 11
+ */
+export interface ExportXML {
+	 (): xmlElement[];
+}
+
+/**
+ * Encodes the xml element.
+ * 
+ * @param element
+ *            The element to be encoded.
+ * @return The encoded element.
+ */
+ export function encodeXML(element: string): string {
+	return element.replace('&', '&amp;').replace('"', '&quot;').replace('\'', '&apos;').replace('<', '&lt;')
+					.replace('>', '&gt;');
+}
+
+/**
+ * Encodes the xml attribute.
+ * 
+ * @param attribute
+ *            The attribute to be encoded.
+ * @return The encoded attribute
+ */
+ export function encodeAttributeXML(attribute: string): string {
+	return attribute.replace('"', '&quot;');
+}
+
